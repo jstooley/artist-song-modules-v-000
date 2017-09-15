40fe500a14ea0050c7655de3e0c5a9baadd11f6b
Justin Tooley <jstooley823@gmail.com>
2017-09-15 07:11:34 +0000

    automatically backed up by learn

--- concerns/memorable.rb
+++ concerns/memorable.rb
@@ -1,12 +1,13 @@
 
 
 module Memorable
-  
-  def self.reset_all
+  module ClassMethods
+    def self.reset_all
      self.all.clear
     end
 
     def self.count
         @@artists.count
     end
+  end
 end
